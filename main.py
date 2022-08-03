6c5ca4043f0316f1ffde0596771f8c1f3292d598
Anych <anuar123@mail.ru>
2022-08-04 05:44:34 +0600

    Add some logic
    Add new functions for exceptions and add logic for learning from russian to english and vice versa

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import random
+from json import JSONDecodeError
 from time import sleep
 from tkinter import *
 from PIL import ImageTk
@@ -51,8 +52,8 @@ class Repeat(Toplevel):
 
         self.overrideredirect(True)
         self.attributes('-topmost', 'true')
-        self.russian_txt = StringVar()
-        self.english_txt = StringVar()
+        self.top_square = StringVar()
+        self.bottom_square = StringVar()
 
         self.save_img = ImageTk.PhotoImage(file="save.ico")
         self.exit_img = ImageTk.PhotoImage(file="exit.ico")
@@ -62,8 +63,8 @@ class Repeat(Toplevel):
 
         self.open_words()
 
-        self.russian = Label(self, width=40, font=('arial', '14', 'bold'), bg='#f3cc5a', textvariable=self.russian_txt)
-        self.english = Label(self, width=40, font=('arial', '14', 'bold'), textvariable=self.english_txt)
+        self.russian = Label(self, width=40, font=('arial', '14', 'bold'), bg='#f3cc5a', textvariable=self.top_square)
+        self.english = Label(self, width=40, font=('arial', '14', 'bold'), textvariable=self.bottom_square)
 
         self.exit = Button(self, image=self.exit_img, relief='ridge', command=lambda: self.quit_pr())
         self.save = Button(self, image=self.save_img, relief='groove', command=lambda: self.save_word())
@@ -97,51 +98,96 @@ class Repeat(Toplevel):
         self.destroy()
 
     def open_words(self):
-        with open('bin/currently_learning.json', 'r') as f:
-            current_words = json.load(f)
-            current_words_length = len(current_words)
+        words = self.try_to_read_json('currently_learning')
+        current_words_length = len(words)
 
-            if current_words_length < 50:
-                self.add_words_to_learning(current_words_length, current_words)
+        if current_words_length < 50:
+            self.add_words_to_learning(current_words_length, words)
 
-            self.next_word(current_words)
+        self.next_word(words)
+        self.set_word()
 
-            self.russian_txt.set(self.value)
+    def set_word(self):
+        with open('bin/learned_from_russian.json', 'r') as f:
+            words = json.load(f)
+            if self.key in words.keys():
+                self.top_square.set(self.key)
+            else:
+                self.top_square.set(self.value)
 
-            thread = threading.Thread(target=self.english_translate)
-            thread.start()
+        thread = threading.Thread(target=self.english_translate)
+        thread.start()
 
     def save_word(self):
-        with open('bin/currently_learning.json', 'wr') as currently_learning:
-            words = json.load(currently_learning)
-            words.pop(self.key)
-            json.dump(words, currently_learning)
-        self.russian_txt.set('')
-        self.english_txt.set('Выучено')
+        words = self.try_to_read_json('learned_from_russian')
+
+        if self.key in words.keys():
+            del words[self.key]
+            self.remove_word_from_learning()
+            learned_words = self.try_to_read_json('learned_words')
+            learned_words[self.key] = self.value
+            self.dump_json('learned_words', learned_words)
+
+        else:
+            words[self.key] = self.value
+
+        self.dump_json('learned_from_russian', words)
+
+        self.top_square.set('')
+        self.bottom_square.set('Выучено')
+
+    def try_to_read_json(self, file_name):
+        with open(f'bin/{file_name}.json', 'r') as f:
+            try:
+                words = json.load(f)
+            except JSONDecodeError:
+                self.dump_json(file_name, {})
+                words = json.load(f)
+
+        return words
 
     def english_translate(self):
         sleep(5)
-        self.english_txt.set(self.key)
+        if self.top_square.get() == self.key:
+            self.bottom_square.set(self.value)
+        else:
+            self.bottom_square.set(self.key)
         sleep(5)
-        self.english_txt.set('')
+        self.bottom_square.set('')
         self.open_words()
 
     def add_words_to_learning(self, current_words_length, current_words):
-        with open('bin/dictionary.json', 'r+') as f:
+        with open('bin/dictionary.json', 'r') as f:
+            dictionary = json.load(f)
             while current_words_length < 50:
-                dictionary = json.load(f)
                 self.next_word(dictionary)
-                current_words[self.key] = self.value
-                dictionary.pop(self.key)
-                current_words_length += 1
-            json.dump(dictionary, f)
+                learned = self.try_to_read_json('learned_words')
+                russian_learned = self.try_to_read_json('learned_from_russian')
+                if self.key not in (current_words, russian_learned, learned):
+                    current_words[self.key] = self.value
+                else:
+                    continue
 
-        with open('bin/currently_learning.json', 'r+') as currently_learning:
-            json.dump(current_words, currently_learning)
+                del dictionary[self.key]
+                current_words_length += 1
+
+        self.dump_json('dictionary', dictionary)
+
+        self.dump_json('currently_learning', current_words)
 
     def next_word(self, dictionary):
         self.key = random.choice(list(dictionary.keys()))
         self.value = dictionary[self.key]
+
+    def remove_word_from_learning(self):
+        words = self.try_to_read_json('currently_learning')
+        del words[self.key]
+        self.dump_json('currently_learning', words)
+
+    @staticmethod
+    def dump_json(file_name, data):
+        with open(f'bin/{file_name}.json', 'w') as f:
+            json.dump(data, f)
 
 
 if __name__ == '__main__':
